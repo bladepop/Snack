b80d1baa4abebb93d11e23491dbbea813b906353
annamat <anna.matveev@gmail.com>
2018-02-11 01:08:31 +0200

    Add message styles

--- app/components/Message/index.js
+++ app/components/Message/index.js
@@ -1,27 +1,75 @@
 /**
-*
-* Message
-*
-*/
+ *
+ * Message
+ *
+ */
 
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import moment from 'moment';
+
+const MessageWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between; 
+  padding: 5px 5px 15px 5px;
+  width: 100%;
+`;
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px; 
+  margin-right: 10px;
+`;
+
+const NameWrapper = styled.div`
+  font-weight: bold;
+  font-family: 'Roboto';
+`;
+
+const DateWrapper = styled.div`
+  font-family: 'Roboto';
+  font-size: 12px;
+  color: gray;
+`;
+
+const ContentWrapper = styled.div`
+  font-family: 'Roboto';
+`;
+
+const ProfileImageWrapper = styled.div`
+  background-image: url("https://yt3.ggpht.com/-JOyKPLnOtAc/AAAAAAAAAAI/AAAAAAAAAAA/fOs3fSwsp0I/s900-c-k-no-mo-rj-c0xffffff/photo.jpg");  
+  border-radius: 50%;
+  height: 60px;
+  width: 60px;   
+  background-size: cover;   
+`;
+
 
 class Message extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     const { author, content, timestamp } = this.props;
     return (
-      <div>
-        <div>
-          <b>{author} </b>
-          <span>{moment(timestamp).fromNow()}</span>
-        </div>
-        <div>
-          {content}
-        </div>
-      </div>
+      <MessageWrapper>
+        <BodyWrapper>
+          <ProfileImageWrapper />
+          <TextWrapper>
+            <NameWrapper>{author}</NameWrapper>
+            <ContentWrapper>
+              {content}
+            </ContentWrapper>
+          </TextWrapper>
+        </BodyWrapper>
+        <DateWrapper>{moment(timestamp).fromNow()}</DateWrapper>
+      </MessageWrapper>
     );
   }
 }
--- app/components/MessageList/index.js
+++ app/components/MessageList/index.js
@@ -6,16 +6,21 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import Message from 'components/Message/Loadable';
+
+const MessagesListWrapper = styled.div`
+  width: 100%;
+`;
+
 
 class MessageList extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     const { messages } = this.props;
     return (
-      <div>
+      <MessagesListWrapper>
         { messages.map((message) => <Message {...message} key={message.id} />) }
-      </div>
+      </MessagesListWrapper>
     );
   }
 }
